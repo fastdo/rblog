86f795c60305aae23c5350884d83f53b3fa421dc
Snowitty <snowitty@163.com>
2023-02-18 17:29:34 +0800

    import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './index.css'
+import { createApi } from 'unsplash-js'
+
+const unsplash = createApi({
+  accessKey: 'YOUR_ACCESS_KEY_HERE',
+})
 
 function App() {
+  const [photos, setPhotos] = useState([])
+
+  useEffect(() => {
+    unsplash.photos
+      .list({ query: 'nature', perPage: 6 })
+      .then((result) => {
+        if (result.errors) {
+          console.log(result.errors[0])
+        } else {
+          setPhotos(result.response.results)
+        }
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">My Blog</h1>
+      <div className="grid grid-cols-2 gap-4">
+        {photos.map((photo) => (
+          <img
+            key={photo.id}
+            src={photo.urls.small}
+            alt={photo.alt_description}
+            className="rounded-lg shadow-lg"
+          />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
